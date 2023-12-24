8ea890c6aaed774c8914d9632767b81eb07ad77a
kwubbenhorst <140316693+kwubbenhorst@users.noreply.github.com>
2023-12-24 04:17:23 -0500

    changes to connection.js file and server.js file to remove console log that would reveal password. Tried to set up a default message at localhost but didn't work so took it out.  Very importantly, solution to my vexacious bug which wouldn't let me connect was to specify USER not as localhost in connection.js but as 127.0.0.1.

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,5 @@
 require('dotenv').config();
 
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
