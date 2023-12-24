6b5a1b7dd3f0ce9d7ea23885c1d7b88f65cd5a84
kwubbenhorst <140316693+kwubbenhorst@users.noreply.github.com>
2023-12-23 19:15:13 -0500

    modified connection.js file to export sequelize with object destructing syntax. This is how I required it in in server.js. Modified server.js to import sequelize connection and to sync sequlize models to the database for creation of tables

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const { sequelize } = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync sequelize models to the database, then turn on the server. This is forcing it for testing, replace later with 
+// Sync Sequelize models to the database
+// sequelize.sync({ force: false }).then(() => {
+//   console.log('Sequelize models synced with the database');
+// });
+sequelize.sync({ force: true }).then(() => {
+  console.log('Sequelize models synced with the database');
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
