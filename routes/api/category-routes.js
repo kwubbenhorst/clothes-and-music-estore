d9567efc1d76e130e37ab43d14b4854592271e5b
kwubbenhorst <140316693+kwubbenhorst@users.noreply.github.com>
2023-12-24 02:31:16 -0500

    content added to category-routes.js and tag-routes.js to complete them

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,80 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// GET all categories
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// GET one category by its `id` value
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!category) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// CREATE a new category
+router.post('/', async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// UPDATE a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const [affectedRows] = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (affectedRows > 0) {
+      const updatedCategory = await Category.findByPk(req.params.id);
+      res.status(200).json(updatedCategory);
+    } else {
+      res.status(404).json({ message: 'No category found with this id!' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// DELETE a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedCategory = await Category.destroy({
+      where: { id: req.params.id },
+    });
+    if (deletedCategory > 0) {
+      res.status(200).json({ message: 'Category deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'No category found with this id!' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const { sequelize } = require('./config/connection');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,12 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server. This is forcing it for testing, replace later with 
-// Sync Sequelize models to the database
-// sequelize.sync({ force: false }).then(() => {
-//   console.log('Sequelize models synced with the database');
-// });
-sequelize.sync({ force: true }).then(() => {
+// sync sequelize models to the database, then turn on the server. 
+sequelize.sync({ force: false }).then(() => {
   console.log('Sequelize models synced with the database');
 });
 
