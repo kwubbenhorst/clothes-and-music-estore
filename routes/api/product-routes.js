bdff5c774563fdde5c8bd062de64c15857ca3487
kwubbenhorst <140316693+kwubbenhorst@users.noreply.github.com>
2023-12-23 22:47:43 -0500

    three files in the routes/api folder were written to perform CRUD operations on my Sequelize models. Sourcecode was most extensive in the product-routes.js file but it all had to be reformulated to use async/await syntax vs. promises. README specs wanted mini-proj for module 13 used as a model for the syntax and it was using async/await. The models column defintions were referred to when figuring out what to include in the req.body

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,97 +3,123 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-});
-
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
-
-// create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
+// GET all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
     });
+    res.status(200).json(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
-// update product
-router.put('/:id', (req, res) => {
-  // update product data
-  Product.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((product) => {
-      if (req.body.tagIds && req.body.tagIds.length) {
-        
-        ProductTag.findAll({
-          where: { product_id: req.params.id }
-        }).then((productTags) => {
-          // create filtered list of new tag_ids
-          const productTagIds = productTags.map(({ tag_id }) => tag_id);
-          const newProductTags = req.body.tagIds
-          .filter((tag_id) => !productTagIds.includes(tag_id))
-          .map((tag_id) => {
-            return {
-              product_id: req.params.id,
-              tag_id,
-            };
-          });
+// GET one product
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+      if (!product) {
+        res.status(404).json({ message: 'No product found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(product);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+});
 
-            // figure out which ones to remove
-          const productTagsToRemove = productTags
+// CREATE new product
+router.post('/', async (req, res) => {
+  try {
+    const { product_name, price, stock, tagIds } = req.body; //There was a comment in the sourcecode specifying what the req.body should look like. Here product_name, price, stock and tagIds have extracted explicitly from 'req.body'to ensure correct fields are included in product creation. 
+
+    // Create the product
+    const product = await Product.create({ product_name, price, stock });
+
+    // If there are tagIds, create pairings in the ProductTag model
+    if (tagIds && tagIds.length) {
+      const productTagIdArr = tagIds.map((tag_id) => ({
+        product_id: product.id,
+        tag_id,
+      }));
+
+      // Bulk create in the ProductTag model
+      await ProductTag.bulkCreate(productTagIdArr);
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+});
+
+
+// UPDATE product
+router.put('/:id', async (req, res) => {
+  try {
+    const [affectedRows] = await Product.update(req.body, {
+      where: { id: req.params.id },
+    });
+
+    if (affectedRows > 0) {
+      if (req.body.tagIds && req.body.tagIds.length) {
+        const productTags = await ProductTag.findAll({
+          where: { product_id: req.params.id },
+        });
+
+        const productTagIds = productTags.map(({ tag_id }) => tag_id);
+        const newProductTags = req.body.tagIds
+          .filter((tag_id) => !productTagIds.includes(tag_id))
+          .map((tag_id) => ({ product_id: req.params.id, tag_id }));
+
+        await ProductTag.bulkCreate(newProductTags);
+
+        const tagsToRemove = productTags
           .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
           .map(({ id }) => id);
-                  // run both actions
-          return Promise.all([
-            ProductTag.destroy({ where: { id: productTagsToRemove } }),
-            ProductTag.bulkCreate(newProductTags),
-          ]);
-        });
+
+        await ProductTag.destroy({ where: { id: tagsToRemove } });
       }
 
-      return res.json(product);
-    })
-    .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
-    });
+      const updatedProduct = await Product.findByPk(req.params.id, {
+        include: [{ model: Category }, { model: Tag, through: ProductTag }],
+      });
+
+      res.status(200).json(updatedProduct);
+    } else {
+      res.status(404).json({ message: 'No product found with this id!' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+//DELETE one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedProduct = await Product.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (deletedProduct > 0) {
+      res.status(200).json({ message: 'Product deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'No product found with this id!' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
+
+//NOTE: this file now represents an update to some pre-existing code.  The pre-existing code was promise based, but the specs for this project requested that a model (mini-proj module 13) be followed for syntax.  There the syntax used async/await, so I have converted all the code blocks accordingly
